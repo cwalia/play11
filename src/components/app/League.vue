e009d640109b2d85169e75a6f315f3a0fa0d783e
Chiranshu Walia <waliachiranshu77@gmail.com>
2022-05-13 23:06:39 +0530

    ui

--- src/components/app/League.vue
+++ src/components/app/League.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="league">
     <div class="navBar navHeader">
       <b-icon icon="chevron-left" @click="back()"></b-icon>
       <div class="flexSpace leagueTeamHead">
@@ -12,18 +12,42 @@
       </div>
       <b-icon icon="wallet" scale="1.5"></b-icon>
     </div>
-    <div class="leagueList">
-      <div class="leagueHead">
-        <img :src="leagueData[0].image">
-        <div class="title">
-          {{leagueData[0].type_name}}
-          <span>{{leagueData[0].slug}}</span>
+    <perfect-scrollbar>
+      <div class="leagueList" v-for="(league,index) in leagueData" :key="index">
+        <div class="leagueHead">
+          <img :src="league.image">
+          <div class="title">
+            {{league.type_name}}
+            <span>{{league.slug}}</span>
+          </div>
+        </div>
+
+        <div class="leagueBody" v-for="(child,key) in league.leagueData" :key="key">
+          <div class="flexSpace leagueBody__head">
+            <div>Prize Pool<br>₹{{child.winning_amount_str}}</div>
+            <div style="text-align:right">Entry Fee<br><div style="display:flex;color:green;align-items:center"><div>{{parseInt(child.max_cash_bonus_used)+parseInt(child.max_referral_bonus_used)}}% bonus</div> <div class="leagueBody__entryFee">₹{{child.entry_fees}}</div></div></div>
+          </div>
+          <div class="leagueBody__body">
+            <b-progress variant="warning" height="8px" :value="child.contest_size-child.spot_left" :max="child.contest_size"></b-progress>
+            <div class="flexSpace" style="margin-top:5px">
+              <div>Only {{child.spot_left}} sports left</div>
+              <div>{{child.contest_size}} Teams</div>
+            </div>
+          </div>
+          <div class="leagueBody__footer">
+            <div class="flexSpace">
+              <div class="flexSpace" style="width:50%">
+                  <div><img src="../../assets/images/league/gold_medal.png">1st {{child.top_prize}}</div>
+                  <div><img src="../../assets/images/league/group.png">Max Team : {{child.contest_size}}</div>
+                  <div><img src="../../assets/images/league/winner_trophy.png">{{child.winners_per}}%</div>
+              </div>
+              <div><span class="leagueBody__mcJoined">{{multiJoined(child.multi_joined)}}</span> <span class="leagueBody__mcJoined">{{cancelContest(child.cancel_contest)}}</span></div>
+            </div>
+          </div>
         </div>
       </div>
-
-      <div class="leagueBody">
-      </div>
-    </div>
+    </perfect-scrollbar>
+    <b-button block variant="primary">Create Team</b-button>
   </div>
 </template>
 
@@ -44,6 +68,12 @@ export default class League extends Vue {
   }
   get leagueData(){
     return this.$store.state.match.leagueData.league
+  }
+  multiJoined(m:any){
+    return m==1?'M':'S'
+  }
+  cancelContest(c:any){
+    return c==1?'U':'C'
   }
 }
 </script>
